2d4018f7afeeb1704a8ec17ee343e3207b0c7c79
caudurodev <rod@cauduro.dev>
2024-11-21 11:56:55 +0100

    fix: ts

--- frontend/src/components/AssertionScore.tsx
+++ frontend/src/components/AssertionScore.tsx
@@ -23,7 +23,7 @@ export const AssertionScore = ({ weightConclusion, proScore = 0, againstScore = 
         <div className={`space-y-2 ${className}`}>
             <div className="flex items-center gap-2">
                 <div className="text-xs text-default-500">Weight:</div>
-                <Progress 
+                <Progress
                     size="sm"
                     radius="sm"
                     classNames={{
@@ -45,22 +45,3 @@ export const AssertionScore = ({ weightConclusion, proScore = 0, againstScore = 
         </div>
     );
 };
-
-export const AssertionComponent = ({ assertion }: AssertionComponentProps) => {
-    const { getAssertionScore } = useScore();
-    const score = getAssertionScore(assertion);
-    console.log('score', score);
-    if (!assertion) return null;
-    return (
-        <div>
-            {/* <h3>{assertion.text}</h3> */}
-            <AssertionScore 
-                weightConclusion={score?.weightConclusion || 0}
-                proScore={score?.pro || 0}
-                againstScore={score?.against || 0}
-            />
-        </div>
-    );
-};
-
-export default AssertionComponent;
